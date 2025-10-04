8e3aaa140554f456e1c9a85eb5f29f017b92bbc2
naoey <naoey@protonmail.ch>
2025-10-04 23:16:55 +0900

    Clean up autogen stuff

--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -1,8 +1,10 @@
 import { type Author } from "@/interfaces/author";
 import Link from "next/link";
-import Avatar from "./avatar";
-import CoverImage from "./cover-image";
 import DateFormatter from "./date-formatter";
+import { Noto_Sans_Display } from "next/font/google";
+import classNames from "classnames";
+
+const notoSans = Noto_Sans_Display();
 
 type Props = {
   title: string;
@@ -23,19 +25,15 @@ export function PostPreview({
 }: Props) {
   return (
     <div>
-      <div className="mb-5">
-        <CoverImage slug={slug} title={title} src={coverImage} />
-      </div>
-      <h3 className="text-3xl mb-3 leading-snug">
+      <h3 className={classNames("text-3xl mb-3 leading-snug text-rose-900", notoSans.className)}>
         <Link href={`/posts/${slug}`} className="hover:underline">
           {title}
         </Link>
       </h3>
-      <div className="text-lg mb-4">
+      <div className="mb-4">
         <DateFormatter dateString={date} />
       </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <Avatar name={author.name} picture={author.picture} />
+      <p className={classNames("text-lg leading-relaxed mb-4", notoSans.className)}>{excerpt}</p>
     </div>
   );
 }
